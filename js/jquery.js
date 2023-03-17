79a2b3bee118bf28db6fe4c7a6b8ca5afafd2f68
Hunter Grubbs <hunter.grubbs07@gmail.com>
2023-03-17 13:41:15 -0500

    mouse events jquery exercise

--- js/jquery.js
+++ js/jquery.js
@@ -1,19 +1,39 @@
 $(function() {
 	"use strict";
 
-	window.onload = function () {
-		alert('The page has finished loading!');
-	}
+	// Exercise 1
+	// window.onload = function () {
+	// 	alert('The page has finished loading!');
+	// }
 
-	alert('Title: ' + $('#page-title').text());
-	alert('Table: ' + $('#table').text());
+	// Exercise 2
+	// alert('Title: ' + $('#page-title').text());
+	// alert('Table: ' + $('#table').text());
 
-	$('.codeup').css({
-		'border': '1px solid red'
-	})
+	// $('.codeup').css({
+	// 	'border': '1px solid red'
+	// })
 
-	$('li').css('font-size', '20px');
-	$('h1, p, li').css('background-color', 'yellow');
-	alert('h1 elements: ' + $('h1').text());
+	// $('li').css('font-size', '20px');
+	// $('h1, p, li').css('background-color', 'yellow');
+	// alert('h1 elements: ' + $('h1').text());
+
+	// Exercise 3
+	$('h1').click(function(e){
+		$('h1').css('background-color', 'orange');
+	});
+
+	$('p').dblclick(function(e){
+		$('p').css('font-size', '18px');
+	});
+
+	$('li').hover(
+		function(e){
+			$('li').css('color', 'red');
+		},
+		function(e){
+			$('li').css('color', 'black');
+		}
+	);
 
 })
